b79b0e8edcaa5eefc5eab1a27c70ff6a0468e3c4
Vinicius Goncalves <vinicius.goncalves@spark.net>
2023-06-09 17:02:28 +0200

    improving Readme and adding requirements

--- README.md
+++ README.md
@@ -3,11 +3,19 @@
 ## Principles
 
 1. Write clear and specific intructions
-2. give time to model think
+2. Give time to model think.
 
-## Libary needed
+    - Specify the steps to complete a task
+    - Instruct the model to work out its own solution before rushing to a conclusion  
+    (Giving time to model doesn't mean asking it to wait 10s to answer, for exemple. Give time to the model is more like being very detaield on the question for the model read, start to recognize the challenge and then start answering.)
+
+## Limitations
+1. Hallucination
+
+
+## Don't forget to install the requirements
 
 ````
-pip3 install openai
+pip3 install -r requirements.txt
 ````
 
--- scripts/first_test.py
+++ scripts/first_test.py
@@ -1,4 +1,38 @@
-import config
+import configparser
 import openai
+import os
 
-openai.api_key = config.openai_key
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+openai.api_key = config['openai']['key']
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+text = f"""
+Athletico is a renowned sports team that excels in various disciplines. 
+With a rich history and dedicated athletes, 
+they have achieved remarkable success on both national and international stages. 
+Known for their unwavering determination and relentless spirit, 
+Athletico has become a symbol of excellence in sports. 
+Their commitment to rigorous training and teamwork has propelled them to victory 
+time and time again. Fans from all corners of the world eagerly 
+await their electrifying performances, cheering them on with unwavering support. 
+Athletico's legacy continues to inspire future generations of athletes, 
+leaving an indelible mark on the sporting world.
+"""
+prompt = f"""
+Summarize the text delimited by triple backticks into a single sentence. 
+Return the answer in a JSON file where the key is the value "answer" and the value is the response
+```{text}```
+"""
+response = get_completion(prompt)
+print(response)
